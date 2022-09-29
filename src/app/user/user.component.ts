9da1e5adfcd18cc17057276950ab46790fabb169
Patrick Sterz <psterz98@gmail.com>
2022-09-29 15:04:05 +0200

    Show users of DB in table

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from '../models/user.class';
@@ -12,11 +13,19 @@ import { User } from '../models/user.class';
 })
 export class UserComponent implements OnInit {
   user = new User();
+  allUsers: any[] = [];
   
 
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog, private firestore: AngularFirestore) { }
 
   ngOnInit(): void {
+    this.firestore
+      .collection('users')
+      .valueChanges()
+      .subscribe((changes: any)=> {
+        console.log('received changes from DB', changes);
+        this.allUsers = changes;
+      })
   }
 
   openDialog() {
