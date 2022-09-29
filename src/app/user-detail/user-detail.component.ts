74d41c837b5046de6f11ea8b2bc35c11fecbee48
Patrick Sterz <psterz98@gmail.com>
2022-09-29 20:08:18 +0200

    get param-route

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-detail',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-detail.component.scss']
 })
 export class UserDetailComponent implements OnInit {
+  userID: any = '';
+  
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe( paramMap => {
+      this.userID = paramMap.get('id');
+      console.log('Got ID:', this.userID)
+    })
   }
 
 }
