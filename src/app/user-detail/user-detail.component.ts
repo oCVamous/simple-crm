836f091dd1b828a016ccb7984cf010250757235e
Patrick Sterz <psterz98@gmail.com>
2022-09-30 09:42:26 +0200

    update

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { user } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { ActivatedRoute } from '@angular/router';
+import { User } from '../models/user.class';
 
 @Component({
   selector: 'app-user-detail',
@@ -8,15 +11,29 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class UserDetailComponent implements OnInit {
   userID: any = '';
+  user: User = new User();
   
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private firestore: AngularFirestore) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe( paramMap => {
       this.userID = paramMap.get('id');
-      console.log('Got ID:', this.userID)
+      console.log('Got ID:', this.userID);
+      this.getUser();
     })
   }
+
+  getUser() {
+    this.firestore
+    .collection('users')
+    .doc(this.userID)
+    .valueChanges()
+    .subscribe((user:any) => {
+      this.user = new User(user);
+      console.log('Retrived user:', this.user);
+    });
+  }
+  
 
 }
